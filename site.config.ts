e295ca0b104c5cbb7f4638816d2d3e0da1a3295b
A010tong <118419669+A010tong@users.noreply.github.com>
2022-12-19 13:40:02 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,24 +2,23 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '11abff265f4b4c7a9ea37f8b8df1ed82',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '古代文论笔记',
+  domain: 'https://flat-mouse-fe7.notion.site/11abff265f4b4c7a9ea37f8b8df1ed82',
+  author: 'A010',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'A010APOD',
+  github: 'A010tong',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
